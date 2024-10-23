d9a744e7501f506ee3350b56b19ef6b66fd45191
Laurenz Roither <lar@wegc203109.uni-graz.at>
2024-10-23 09:45:05 +0200

    uniform changes to sst work

--- apply_on_forcing_data/apply_sst.py
+++ apply_on_forcing_data/apply_sst.py
@@ -1,3 +1,4 @@
+
 import xarray as xr
 import numpy as np
 import argparse
@@ -20,28 +21,32 @@ python apply_CC_cas.py --cas_input_dir=/path/to/cas_files --output_dir=/path/to/
 """
 
 
-def process_file(cas_file_path, sst_file_path, output_dir):
+def process_file(cas_file, cas_file_path, output_dir):
     #print(cc_signal_file_path)
-    file_cas = xr.open_dataset(cas_file_path)
+    file_cas_original = xr.open_dataset(cas_file_path/cas_file)
 
-    fr_land = file_cas['FR_LAND']
+    ts = file_cas_original['T_SKIN'] - 1.3
+    fr_land = file_cas_original['FR_LAND']
 
-    file_cas['T_SKIN'] = xr.where(fr_land <= 0.05, file_cas['T_SKIN'] - 1.3, file_cas['T_SKIN'])
+    file_cas_new = xr.open_dataset(cas_file_path)
+
+    file_cas_new['T_SKIN'] = xr.where(fr_land <= 0.05, ts, file_cas_new['T_SKIN'])
 
     output_file_path = Path(output_dir) / cas_file_path.name
-    file_cas.to_netcdf(output_file_path)
+    file_cas_new.to_netcdf(output_file_path)
 
 def main():
     parser = argparse.ArgumentParser(description='Process SST and CC signal files.')
+    parser.add_argument('--cas_file', required=True, help='Input CAS files (specified or with placeholders such as cas*.nc).')
     parser.add_argument('--cas_input_dir', required=True, help='Input directory for CAS files.')
     parser.add_argument('--output_dir', required=True, help='Output directory for modified CAS files.')
     args = parser.parse_args()
 
     
-    cas_files = glob.glob(f'{args.cas_input_dir}/cas*[0-9].nc')
+    cas_files = glob.glob(f'{args.cas_input_dir}/{args.cas_file}')
 
     for cas_file in cas_files:
-        process_file(Path(cas_file), Path(args.sst_file_path), Path(args.output_dir))
+        process_file(Path(cas_file), Path(args.output_dir))
 
 if __name__ == "__main__":
     main()
--- apply_on_forcing_data/apply_sst.sh
+++ apply_on_forcing_data/apply_sst.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 #SBATCH --job-name=CREATE_COUNTERFACTUAL_sst
-#SBATCH --time=05:00:00
+#SBATCH --time=00:10:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=128   
 #SBATCH --ntasks-per-core=1
 #SBATCH --partition=zen3_0512
-#SBATCH --qos=zen3_0512
+#SBATCH --qos=zen3_0512_devel
 #SBATCH -A p72281
 
 echo "Current working directory: $(pwd)"
@@ -15,7 +15,7 @@ echo "Ensuring output directory ${3} exists..."
 mkdir -p ${3}
 
 # Define variables
-#GCM_NAME=$2
+CAS_FILE=$1
 INPUT_DIR_CAS=$2
 OUTPUT_DIR_CAS=$3
 
@@ -37,28 +37,8 @@ do
     fi
     
     echo "Processing file: ${FILE}"
-    python apply_sst.py --cas_input_dir=${INPUT_DIR_CAS} --output_dir=${OUTPUT_DIR_CAS}
+    python apply_sst.py --cas_file=${cas_file} --cas_input_dir=${INPUT_DIR_CAS} --output_dir=${OUTPUT_DIR_CAS}
     echo "Done processed ${FILE}"
 done
 
 
-# Change directory to the output directory where the processed files are
-cd ${OUTPUT_DIR_CAS}
-
-# Print the directory you're working in now
-echo "Now working in directory: $(pwd)"
-
-# Loop through each .nc file in the output directory
-for FILE in ${FILES}; 
-do 
-    FILE=$(basename ${FILE})
-    echo "Appending variables from original file to: ${FILE}"
-
-    # Determine the original file path
-    original_file="/gpfs/data/fs72281/lar/change_temp/CC_signals_Laurenz/${FILE:0:3}2023${FILE:7}"
-
-    # Append variables from the original file
-    ncks -A -v akm,bkm,time_bnds "${original_file}" "${FILE}"
-done
-
-echo "Appended variables from original file!"
